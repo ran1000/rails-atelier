57b0ac69af5624d63c46d594a957af8a05561eb1
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-22 22:34:02 +0200

    heroku setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+- Atelier -
+
+An app for collecting moments, projects, ideas, thoughts and curated fragments of life. The idea of a never ending work-in-progress space, where things cross by and meet up.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # LINE added after minimal template >> change host and port to heroku
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
