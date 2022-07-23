3686e42ac6c7b45d654d80051f202d1c54f05d37
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-23 21:28:53 +0200

    post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/q', as: 'rails_admin'
   root to: "pages#home"
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts
 
   # Defines the root path route ("/")
   # root "articles#index"
