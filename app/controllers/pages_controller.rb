6281172269c6b6811e323323b371521c9f37935d
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-24 18:07:18 +0200

    link to categories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+    tags = ["art", "inspiration", "notes", "projects", "todigital"]
+    if tags.include?(params[:tag])
+      params[:tag] = params[:tag]
+    else
+      params[:tag] = "Tag not included"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/q', as: 'rails_admin'
-  root to: "pages#home"
+  root to: "pages#home", as: :home
   get 'posts/index'
   get 'posts/show'
   get 'posts/new'
   get 'posts/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts
+
+  get 'about/:field', to: 'pages#about', as: :about
 
   # Defines the root path route ("/")
   # root "articles#index"
