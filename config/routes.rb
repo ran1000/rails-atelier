60c4517878645e41b53c0bbf09defcfc4448d8f3
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-26 16:49:41 +0200

    design navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/q', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/admin', as: 'admin'
   root to: "pages#home", as: :home
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :posts
-
+  resources :posts, only: [:index]
   get 'about/:tag', to: 'pages#about', as: :about
 
   # Defines the root path route ("/")
