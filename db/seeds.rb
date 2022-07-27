eb974e97d796da6302c0a592374f76a0e255fe55
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-07-27 10:39:50 +0200

    fix logic seeding and refactoring categories seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,24 @@
 #
 # movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 # Character.create(name: "Luke", movie: movies.first)
-unless Post.first
+unless Post.first && Category.first && PostsCategory.first
   # Posts Seeds
   puts "Creating posts..."
   norberg01_post = { title: "Norberg01", text: "Norberg 2022", images: "https://images.unsplash.com/photo-1514272272587-d4fae170976f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=974&q=80" }
   writing01_post = { title: "WritingAct01", text: "Lorem Ipsum" }
   [norberg01_post, writing01_post].each do |attributes|
     post = Post.create!(attributes)
-    puts "Created #{post.name}"
+    puts "Created #{post.title.downcase} post"
   end
 
   # Categories Seeds
   puts "Creating categories..."
-  places_category = Category.new(tag: "Places")
-  places_category.save!
-  notes_category = Category.new(tag: "Notes")
-  notes_category.save!
+  places_category = { tag: "Places"}
+  notes_category = { tag: "Notes" }
+  [places_category, notes_category].each do |attributes|
+    category = Category.create!(attributes)
+    puts "Created #{category.tag.downcase} category"
+  end
 
   # Posts-Categories
   puts "Creating posts-categories ..."
