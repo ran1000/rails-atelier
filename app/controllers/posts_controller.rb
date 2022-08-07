bdf20233da8ffc07102b47e65310f78db4ba54da
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-08-07 16:06:40 +0200

    add show action to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,20 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :find_post, only: :show
 
   def index
     @posts = Post.all
   end
 
+  def show
+  end
+
   def category
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: "posts#index", as: :home
-  # get 'posts/show'
-  # get 'posts/new'
-  # get 'posts/edit'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index] do
+
+  resources :posts, only: %i[index show] do
     collection do
       get :category
     end
   end
   get 'about/:tag', to: 'pages#about', as: :about
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
