41d0c99b88432a0919bd29cf3e66ed049cc6259f
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-11-08 21:04:44 +0100

    refactor js grid controller in stimulus and fixed bug link_to

--- app/javascript/controllers/grid_controller.js
+++ app/javascript/controllers/grid_controller.js
@@ -1,42 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
 import imagesLoaded from "imagesloaded";
 
+// Dynamic Row Span
 // Connects to data-controller="grid"
 export default class extends Controller {
+  static targets = ["items"]
+
+  resizeAllGridItems() {
+    Array.from(this.itemsTargets).forEach((item) => {
+      this.resizeGridItem(item);
+    });
+  };
+
+  resizeInstance(instance) {
+    const item = instance.elements[0];
+    this.resizeGridItem(item);
+  };
+
+  resizeGridItem(item) {
+    const rowHeight = parseInt(window.getComputedStyle(this.element).getPropertyValue("grid-auto-rows"));
+    const rowGap = parseInt(window.getComputedStyle(this.element).getPropertyValue("grid-row-gap"));
+    const rowSpan = Math.ceil((item.querySelector(".content").getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+    item.style.gridRowEnd = "span " + rowSpan;
+  };
+
   connect() {
-    console.log("Hello from Stimulus Grid");
-
-    // Dynamic Row Span
-    // const allItems = document.getElementsByClassName("item");
-
-    // function resizeGridItem(item) {
-    //   const grid = document.querySelector(".grid");
-    //   const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue("grid-auto-rows"));
-    //   const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue("grid-row-gap"));
-    //   const rowSpan = Math.ceil((item.querySelector(".content").getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
-    //   item.style.gridRowEnd = "span " + rowSpan;
-    // }
-
-    // function resizeAllGridItems(items) {
-    //   Array.from(items).forEach((item) => {
-    //     resizeGridItem(item);
-    //   });
-    // }
-
-    // function resizeInstance(instance) {
-    //   const item = instance.elements[0];
-    //   resizeGridItem(item);
-    // }
-
-    // window.onload = resizeAllGridItems(allItems);
-    // window.addEventListener("resize", resizeAllGridItems(allItems));
-    // Array.from(allItems).forEach((item) => {
-    //   // This function passes the items on the resizeInstance after loading the image
-    //   imagesLoaded(item, resizeInstance);
-    // });
+    this.updateGrid();
+    Array.from(this.itemsTarget).forEach((item) => {
+      // This function passes the items on the resizeInstance after loading the image
+      imagesLoaded(item, resizeInstance);
+    });
   }
 
   updateGrid() {
-    console.log("resizing");
+    this.resizeAllGridItems();
   }
 }
