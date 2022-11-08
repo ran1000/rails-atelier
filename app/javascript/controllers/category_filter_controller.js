6970a2919e78d78b6dad304480a6ab61fb72ad63
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-11-08 23:38:21 +0100

    implement stimulus category filter

--- app/javascript/controllers/category_filter_controller.js
+++ app/javascript/controllers/category_filter_controller.js
@@ -5,16 +5,27 @@ export default class extends Controller {
   static targets = ["items"];
   // filter using classes and hiding others
   connect() {
-    console.log("Hello from category filter");
   }
 
-  updateItems() {
-    console.log("Updating");
+  isActive(selection, itemCat) {
+    return itemCat.includes(selection);
+  }
+
+  updateItems(event) {
+    const selectedCat = event.currentTarget.dataset.category
     this.itemsTargets.forEach((item) => {
-      console.dir(this.itemsTarget);
+      const cardCat = item.dataset.category;
+      if (!this.isActive(selectedCat, cardCat)) {
+        // item.classList.add("hidden");
+        item.setAttribute("hidden", "");
+      } else {
+        item.removeAttribute("hidden");
+        // item.classList.remove("hidden");
+      }
     });
   }
 
+  // Alternative Solution
   // the page is a white canvas
   // fetch all the posts once connecting
   // fetch just a category on clicking the category
