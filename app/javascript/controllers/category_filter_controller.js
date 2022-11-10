9c240bc5a64428ebf064a730016406e6881e2239
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-11-10 16:30:00 +0100

    connecting two stimulus controllers

--- app/javascript/controllers/category_filter_controller.js
+++ app/javascript/controllers/category_filter_controller.js
@@ -2,8 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="category-filter"
 export default class extends Controller {
-  static targets = ["items"];
-  // filter using classes and hiding others
+  static targets = ["items", "children"];
+
+  // sendMessageToChild() {
+  //   this.childrenTarget.dispatchEvent(new CustomEvent("message", { message: "Hello" }))
+  // }
+
   connect() {
   }
 
@@ -16,17 +20,17 @@ export default class extends Controller {
     this.itemsTargets.forEach((item) => {
       const cardCat = item.dataset.category;
       if (!this.isActive(selectedCat, cardCat)) {
-        // item.classList.add("hidden");
         item.setAttribute("hidden", "");
       } else {
         item.removeAttribute("hidden");
-        // item.classList.remove("hidden");
       }
     });
   }
 
-  // Alternative Solution
-  // the page is a white canvas
-  // fetch all the posts once connecting
-  // fetch just a category on clicking the category
+
 }
+
+// Alternative Solution
+// the page is a white canvas
+// fetch all the posts once connecting
+// fetch just a category on clicking the category
